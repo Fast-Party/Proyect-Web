ccbefba63bad37a551f6617c1e0fae944c2a7e28
Albert <albertbatcas@campus.monlau.com>
2023-10-23 19:22:11 +0200

    finishingLogin

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
@@ -8,7 +9,7 @@ function Login() {
         </h1>
         <div className='inputBox'>
                 <form>
-                    <h3>Sign In</h3>
+                    <h3 className='signIn'>Sign In</h3>
                     <hr className='titleSeparation'/>
                     <div className='mb-2'>
                         <input placeholder='Username' className='username'></input>
@@ -22,14 +23,17 @@ function Login() {
                             LOGIN
                         </button>
                     </div>
-                    <p className='text-right'>
-                        Forgot your <a href=''>Password?</a>
-                        <br />
-                        <a href=''>Sign up</a>
+                    <p className='changeRegister'>
+                        You dont have an account? <a href='' className='linkChangeRegister'>Register</a>
+                        <br/>
                     </p>
+                    <p href='' className='forgetPassword'>
+                        Forgot your Password?
+                    </p>
+                        
+                    
                 </form>
             </div>
-
             <svg preserveAspectRatio="xMidYMid slice" viewBox="10 10 80 80">
                 <path fill="#d46bd0" fillOpacity={"100%"} class="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z" />
                 <path fill="#e061d9" fillOpacity={"70%"} class="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z" />
